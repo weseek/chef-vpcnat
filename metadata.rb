0cdc5856b25aa33ea801d89daa51488b6bd48e38
Yuki Takei <yuki@weseek.co.jp>
2014-04-14 13:50:51 +0900

    update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'yuki@weseek.co.jp'
 license          'All rights reserved'
 description      'Setup NAT instances on Amazon EC2/VPC'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 depends 'awscli'
 depends 'cron'
