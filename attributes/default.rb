9d5ac185faf4a8ba633edf810ea9e43ee4f00229
Yuki Takei <yuki@weseek.co.jp>
2014-04-22 18:09:54 +0900

    clean attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,3 @@ default["vpcnat"]["scripts"]["group"] = "root"
 default["vpcnat"]["scripts"]["install_dir"] = "/opt/heartbeat"
 default["vpcnat"]["scripts"]["aws_fullpath"] = "/usr/local/bin/aws"
 
-default["vpcnat"]["sns_arn"] = ""
-default["vpcnat"]["sns_region"] = ""
