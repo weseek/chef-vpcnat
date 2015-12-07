d3fde4235498117bc9b4e3dfe8b78eb634225f79
Koki Oyatsu <kouki@weseek.co.jp>
2015-12-07 10:35:03 +0900

    sysctl deps update

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Setup NAT instances on Amazon EC2/VPC'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.5'
 
-depends 'sysctl', '= 0.4.0'
+depends 'sysctl', '= 0.7.0'
 depends 'cron', '= 1.3.6'
 depends 'awscli'
 depends 'iptables'
